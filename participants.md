8628833242f83f9ce3ef0e905e46ecef128213c5
Janani <janani.sundar@students.iiit.ac.in>
2015-08-05 01:42:37 +0530

    Eighth commit - after correcting file names

--- participants.md
+++ participants.md
@@ -3,5 +3,5 @@ A list of participants in CSE 602
 
 *Please keep this list in alphabetical order by name.*
 
-* Gugan Vignesh Selvaraj
-* Janani Sundar
+* Gugan Vignesh Selvaraj (201212345)
+* Janani Sundar (201460623)
--- participants.rkt
+++ participants.rkt
@@ -1,15 +1,17 @@
  ; participants.scm
- ;   A simple scheme program to print the participants from the class.
+ ;   A simple scheme program to print the participants' names and roll numbers from the class.
  ;   Intended as an example of updating code on GitHub.
 
   (define i 0)
-  (define (prnnames na)
+  (define (prnnames no na)
    (set! i (+ i 1))
    (display i)
+   (display " ")
+   (display no)
    (display " ")
    (display na)
    (display "\n")
   )
 
-  (prnnames "Gugan Vignesh Servaraj")
-  (prnnames "Janani Sundar")
+  (prnnames "201212345" "Gugan Vignesh Servaraj")
+  (prnnames "201460623" "Janani Sundar")
