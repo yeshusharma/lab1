caa597f09fc5fa29fa31bec7cb49033368c71c95
gitanjali vaidya <gitanjali.v@students.iiit.ac.in>
2015-08-08 11:59:19 +0530

    Added by gitanjali as part of lab ex1

--- participants.rkt
+++ participants.rkt
@@ -15,6 +15,7 @@
 
   (prnnames "201560543" "Aditi Gautam")
   (prnnames "201560562" "Anjali Verma")
+  (prnnames "201560558" "Gitanjali Vaidya")
  (prnnames "201212345" "Gugan Vignesh Servaraj")
   (prnnames "201460623" "Janani Sundar")
   (prnnames "201560557" "Sudhanshu Kumar Singh")
